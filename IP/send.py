05928ab50fb5fcdcc72d6ff2f0afed4e51791849
riccardoprosdocimi <riccardo.prosdocimi@gmail.com>
2022-10-26 17:43:52 -0400

    modified checksum

--- IP/send.py
+++ IP/send.py
@@ -11,36 +11,16 @@ def csum(sent_message, nbytes):
     :return:
     """
 
-    # 96 bit (12 bytes) pseudo header needed for tcp header checksum calculation
-    source_address = sent_message[0:nbytes]
-    dest_address = sent_message[nbytes:2 * nbytes]
-    placeholder = sent_message[2 * nbytes:3 * nbytes]
-    protocol = sent_message[3 * nbytes: 4 * nbytes]
-    tcp_length = sent_message[4 * nbytes:5 * nbytes]
-
-    # calculating the binary sum of packets
-    binary_sum = bin(int(source_address, 2) +
-                     int(dest_address, 2) +
-                     int(placeholder, 2) +
-                     int(protocol, 2) +
-                     int(tcp_length, 2))[2:]
-
-    # adding the overflow bits
-    if len(binary_sum) > nbytes:
-        of_bits = len(binary_sum) - nbytes
-        binary_sum = bin(int(binary_sum[0:of_bits], 2) +
-                         int(binary_sum[of_bits:], 2))[2:]
-    if len(binary_sum) < nbytes:
-        binary_sum = '0' * (nbytes - len(binary_sum)) + binary_sum
-
-    # calculating the complement of sum
-    checksum = ''
-    for i in binary_sum:
-        if i == '1':
-            checksum += '0'
-        else:
-            checksum += '1'
-
+    checksum = 0
+    while nbytes > 1:
+        checksum += sent_message
+        nbytes -= 2
+    if nbytes == 1:
+        oddbyte = sent_message
+        checksum += oddbyte
+    checksum = (checksum >> 16) + (checksum & 0xffff)
+    checksum = checksum + (checksum >> 16)
+    checksum = ~checksum
     return checksum
 
 
