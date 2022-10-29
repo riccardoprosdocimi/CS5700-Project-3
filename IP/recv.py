21aab00244efc2090c27cda03590506749349c89
Rohit Awate <rohitawate121@gmail.com>
2022-10-29 21:37:30 +0000

    Able to receive packets!

--- IP/recv.py
+++ IP/recv.py
@@ -3,8 +3,18 @@
 import socket
 
 
-HOST = socket.gethostbyname(socket.gethostname())
-print(HOST)
-sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.IPPROTO_IP)
-sock.bind((HOST, 0))
-print(sock.recv(2048))
+def get_nw_interface_name() -> str:
+    prefixes = ("enp", "eth")
+
+    for _, int_name in socket.if_nameindex():
+        for prefix in prefixes:
+            if int_name.startswith(prefix):
+                return int_name
+
+    raise ValueError("Cannot find a valid network interface")
+
+
+ip_proto = socket.htons(0x0800)
+sock = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, ip_proto)
+pkt = sock.recvfrom(2048)
+print(pkt)
