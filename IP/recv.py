1471ad280bbe3b863a7560bd5c1a094680a9e376
Rohit Awate <rohitawate121@gmail.com>
2022-10-29 21:38:17 +0000

    Move get_nw_interface_name to utils.py

--- IP/recv.py
+++ IP/recv.py
@@ -3,17 +3,6 @@
 import socket
 
 
-def get_nw_interface_name() -> str:
-    prefixes = ("enp", "eth")
-
-    for _, int_name in socket.if_nameindex():
-        for prefix in prefixes:
-            if int_name.startswith(prefix):
-                return int_name
-
-    raise ValueError("Cannot find a valid network interface")
-
-
 ip_proto = socket.htons(0x0800)
 sock = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, ip_proto)
 pkt = sock.recvfrom(2048)
