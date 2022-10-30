3152e40a86cbae020841a7997cc457ae8508f3d9
Rohit Awate <rohitawate121@gmail.com>
2022-10-30 01:38:38 +0000

    Build an entire IPPacket object

--- IP/__init__.py
+++ IP/__init__.py
@@ -1,10 +1,9 @@
-class IPHeader:
-
-    def __init__(self, total_length, source, destination):
+class IPPacket:
+    def __init__(self, source, destination, data):
         self.version = 4
         self.header_length = 5
         self.service_type = 0
-        self.total_length = total_length
+        self.total_length = len(data) + 20
         self.identification = 0
         self.flags = 0
         self.fragment_offset = 0
--- IP/recv.py
+++ IP/recv.py
@@ -4,14 +4,40 @@ from binascii import hexlify
 import socket
 import struct
 
+from IP import IPPacket
 
 ip_proto = socket.htons(0x0800)
 sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, ip_proto)
-pkt = sock.recvfrom(2048)[0]
 
-raw_ip_header = pkt[18:34]
-print(hexlify(raw_ip_header))
+while True:
+    pkt = sock.recvfrom(4096)[0]
 
-row_2 = raw_ip_header[:2]
-id = struct.unpack("!2s", row_2)
-print(hexlify(id[0]))
+    raw_ip_header = pkt[14:35]
+
+    total_length_raw = raw_ip_header[2:4]
+    (total_length,) = struct.unpack("!h", total_length_raw)
+    data = pkt[35 : 35 + total_length - 20]  # header length = 20
+
+    pkt_id_raw = raw_ip_header[5:7]
+    (pkt_id,) = struct.unpack("!h", pkt_id_raw)
+
+    ttl_raw = raw_ip_header[9:10]
+    (ttl,) = struct.unpack("!c", ttl_raw)
+    ttl = int(hexlify(ttl), base=16)
+
+    protocol_raw = raw_ip_header[10:11]
+    (protocol,) = struct.unpack("!c", protocol_raw)
+    protocol = int(hexlify(protocol), base=16)
+
+    checksum_raw = raw_ip_header[11:13]
+    (checksum,) = struct.unpack("!h", checksum_raw)
+
+    src_ip_raw = raw_ip_header[13:17]
+    src_ip = socket.inet_ntoa(src_ip_raw)
+    print(src_ip)
+
+    dst_ip_raw = raw_ip_header[17:21]
+    dst_ip = socket.inet_ntoa(dst_ip_raw)
+
+    ip_packet = IPPacket(src_ip, dst_ip, data)
+    print(ip_packet)
