83680b594a38b0f84a36d3e4647b9b65bd250851
Rohit Awate <rohitawate121@gmail.com>
2022-10-30 00:50:12 +0000

    Print packet ID

--- IP/recv.py
+++ IP/recv.py
@@ -1,9 +1,17 @@
 # Rohit
 
+from binascii import hexlify
 import socket
+import struct
 
 
 ip_proto = socket.htons(0x0800)
-sock = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, ip_proto)
-pkt = sock.recvfrom(2048)
-print(pkt)
+sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, ip_proto)
+pkt = sock.recvfrom(2048)[0]
+
+raw_ip_header = pkt[18:34]
+print(hexlify(raw_ip_header))
+
+row_2 = raw_ip_header[:2]
+id = struct.unpack("!2s", row_2)
+print(hexlify(id[0]))
