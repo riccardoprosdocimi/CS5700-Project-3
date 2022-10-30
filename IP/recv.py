28740f055dd9205f1ac62002d7c3038821dfe7fc
Rohit Awate <rohitawate121@gmail.com>
2022-10-30 01:44:23 +0000

    Build full IP packet and call from main

--- IP/recv.py
+++ IP/recv.py
@@ -9,35 +9,38 @@ from IP import IPPacket
 ip_proto = socket.htons(0x0800)
 sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, ip_proto)
 
-while True:
-    pkt = sock.recvfrom(4096)[0]
+def start():
+    while True:
+        pkt = sock.recvfrom(4096)[0]
 
-    raw_ip_header = pkt[14:35]
+        raw_ip_header = pkt[14:35]
 
-    total_length_raw = raw_ip_header[2:4]
-    (total_length,) = struct.unpack("!h", total_length_raw)
-    data = pkt[35 : 35 + total_length - 20]  # header length = 20
+        total_length_raw = raw_ip_header[2:4]
+        (total_length,) = struct.unpack("!h", total_length_raw)
+        data = pkt[35 : 35 + total_length - 20]  # header length = 20
 
-    pkt_id_raw = raw_ip_header[5:7]
-    (pkt_id,) = struct.unpack("!h", pkt_id_raw)
+        pkt_id_raw = raw_ip_header[5:7]
+        (pkt_id,) = struct.unpack("!h", pkt_id_raw)
 
-    ttl_raw = raw_ip_header[9:10]
-    (ttl,) = struct.unpack("!c", ttl_raw)
-    ttl = int(hexlify(ttl), base=16)
+        ttl_raw = raw_ip_header[9:10]
+        (ttl,) = struct.unpack("!c", ttl_raw)
+        ttl = int(hexlify(ttl), base=16)
 
-    protocol_raw = raw_ip_header[10:11]
-    (protocol,) = struct.unpack("!c", protocol_raw)
-    protocol = int(hexlify(protocol), base=16)
+        protocol_raw = raw_ip_header[10:11]
+        (protocol,) = struct.unpack("!c", protocol_raw)
+        protocol = int(hexlify(protocol), base=16)
 
-    checksum_raw = raw_ip_header[11:13]
-    (checksum,) = struct.unpack("!h", checksum_raw)
+        checksum_raw = raw_ip_header[11:13]
+        (checksum,) = struct.unpack("!h", checksum_raw)
 
-    src_ip_raw = raw_ip_header[13:17]
-    src_ip = socket.inet_ntoa(src_ip_raw)
-    print(src_ip)
+        src_ip_raw = raw_ip_header[13:17]
+        src_ip = socket.inet_ntoa(src_ip_raw)
 
-    dst_ip_raw = raw_ip_header[17:21]
-    dst_ip = socket.inet_ntoa(dst_ip_raw)
+        dst_ip_raw = raw_ip_header[17:21]
+        dst_ip = socket.inet_ntoa(dst_ip_raw)
 
-    ip_packet = IPPacket(src_ip, dst_ip, data)
-    print(ip_packet)
+        ip_packet = IPPacket(src_ip, dst_ip, data)
+        ip_packet.identification = pkt_id
+        ip_packet.ttl = ttl
+        ip_packet.protocol = protocol
+        ip_packet.checksum = checksum
