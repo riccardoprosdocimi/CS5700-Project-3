0dbc45658904e62db000446209481b7a3c1eabe8
Rohit Awate <rohitawate121@gmail.com>
2022-10-31 16:01:59 +0000

    Fix IP packet parsing offsets

--- IP/__init__.py
+++ IP/__init__.py
@@ -82,24 +82,24 @@ class IPPacket:
         (total_length,) = struct.unpack("!h", total_length_raw)
         data = raw_pkt[35 : 35 + total_length - 20]  # header length = 20
 
-        pkt_id_raw = raw_ip_header[5:7]
+        pkt_id_raw = raw_ip_header[4:6]
         (pkt_id,) = struct.unpack("!h", pkt_id_raw)
 
-        ttl_raw = raw_ip_header[9:10]
+        ttl_raw = raw_ip_header[8:9]
         (ttl,) = struct.unpack("!c", ttl_raw)
         ttl = int(hexlify(ttl), base=16)
 
-        protocol_raw = raw_ip_header[10:11]
+        protocol_raw = raw_ip_header[9:10]
         (protocol,) = struct.unpack("!c", protocol_raw)
         protocol = int(hexlify(protocol), base=16)
 
-        checksum_raw = raw_ip_header[11:13]
+        checksum_raw = raw_ip_header[10:12]
         (checksum,) = struct.unpack("!h", checksum_raw)
 
-        src_ip_raw = raw_ip_header[13:17]
+        src_ip_raw = raw_ip_header[12:16]
         src_ip = socket.inet_ntoa(src_ip_raw)
 
-        dst_ip_raw = raw_ip_header[17:21]
+        dst_ip_raw = raw_ip_header[16:20]
         dst_ip = socket.inet_ntoa(dst_ip_raw)
 
         ip_packet = IPPacket(src_ip, dst_ip, data)
@@ -109,3 +109,5 @@ class IPPacket:
         ip_packet.checksum = checksum
 
         return ip_packet
+
+PROTOCOLS_TO_CAPTURE = (socket.IPPROTO_TCP, socket.IPPROTO_UDP)
