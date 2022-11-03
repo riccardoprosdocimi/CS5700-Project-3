7625c53890137f275f22b98f852d6c38a6813ac6
riccardoprosdocimi <riccardo.prosdocimi@gmail.com>
2022-11-02 23:13:20 -0400

    fixed checksum

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import socket
-import array
+import struct
 
 
 def get_nw_interface_name() -> str:
@@ -19,32 +19,12 @@ def get_local_ip():
 
 
 def csum(packet):
+    checksum = 0  # initialize checksum to zero
+    words = len(packet) // 2
+    for chunk in struct.unpack("!%sH" % words, packet[:words * 2]):
+        checksum += chunk  # add up 16-bit words
     if len(packet) % 2 != 0:
-        packet += b'\0'
-    checksum = sum(
-        array.array("H", packet)  # create array of fixed element types to calculate sum of 16-bit words
-    )
-    checksum = (checksum >> 16) + (checksum & 0xFFFF)
-    checksum += checksum >> 16
-    return ~checksum & 0xFFFF
-
-
-# def csum(self, sent_message, nbytes):
-#     """
-#     Generic checksum calculation function.
-#
-#     :param sent_message:
-#     :param nbytes:
-#     :return:
-#     """
-#
-#     while nbytes > 1:
-#         self.checksum += sent_message
-#         nbytes -= 2
-#     if nbytes == 1:
-#         oddbyte = sent_message
-#         self.checksum += oddbyte
-#     self.checksum = (self.checksum >> 16) + (self.checksum & 0xFFFF)
-#     self.checksum = self.checksum + (self.checksum >> 16)
-#     self.checksum = ~self.checksum
-#     return self.checksum
+        checksum += b'\0'  # add leftover byte
+    checksum = (checksum >> 16) + (checksum & 0xffff)
+    checksum += checksum >> 16  # fold 32-bit into 16-bit
+    return ~checksum & 0xffff
