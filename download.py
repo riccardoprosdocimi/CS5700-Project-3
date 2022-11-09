5d8b80a7872de0defb260edbf7aea676d3fa8344
Rohit Awate <rohitawate121@gmail.com>
2022-11-09 18:45:32 -0500

    Fix for paths ending with slash or no slash

--- download.py
+++ download.py
@@ -2,7 +2,6 @@ import sys
 from typing import Tuple
 from urllib.parse import urlparse
 from tcp_sock import TCPSocket
-from http_req import HttpRequest
 from data import Data
 
 
@@ -18,8 +17,8 @@ def download(url: str):
         print("Handshake failed", file=sys.stderr)
         sys.exit(1)
 
-    if path == "/":
-        path = ""
+    if path == "":
+        path = "/"
 
     get_req = Data(dst_host, path)
     http_req = get_req.build_get_message()
@@ -27,13 +26,3 @@ def download(url: str):
     data = tcp_socket.recv()
     get_req.get_content_type(data)
     get_req.save_file()
-
-
-
-    # get_req = HttpRequest(target=path)
-    # get_req.header("Host", dst_host)
-    # http_req = get_req.build()
-    # print(http_req)
-    # tcp_socket.send(http_pkt=http_req)
-    # data = tcp_socket.recv()
-    # print(data.decode())
