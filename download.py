53aa65748796a1487bac4cb4069e970f83fbebc5
riccardoprosdocimi <riccardo.prosdocimi@gmail.com>
2022-11-08 23:27:30 -0500

    implemented riccardo's http

--- download.py
+++ download.py
@@ -1,7 +1,9 @@
+import sys
 from typing import Tuple
 from urllib.parse import urlparse
 from tcp_sock import TCPSocket
 from http_req import HttpRequest
+from data import Data
 
 
 def get_url_components(url_str: str) -> Tuple[str, str]:
@@ -13,16 +15,26 @@ def download(url: str):
     dst_host, path = get_url_components(url)
     tcp_socket = TCPSocket(dst_host=dst_host)
     if not tcp_socket.connect():
-        print("Handshake failed!")
-        # TODO: print error and sys.exit
+        print("Handshake failed", file=sys.stderr)
+        sys.exit(1)
 
     if path == "/":
         path = ""
 
-    get_req = HttpRequest(target=path)
-    get_req.header("Host", dst_host)
-    http_req = get_req.build()
-
-    tcp_socket.send(http_pkt=http_req)
+    get_req = Data(dst_host, path)
+    http_req = get_req.build_get_message()
+    tcp_socket.send(http_req)
+    print(get_req.request)
     data = tcp_socket.recv()
-    print(data.decode())
+    get_req.get_content_type(data)
+    get_req.save_file()
+
+
+
+    # get_req = HttpRequest(target=path)
+    # get_req.header("Host", dst_host)
+    # http_req = get_req.build()
+    # print(http_req)
+    # tcp_socket.send(http_pkt=http_req)
+    # data = tcp_socket.recv()
+    # print(data.decode())
